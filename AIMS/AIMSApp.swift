f5e8b0b3eaa8d6728458a94f5d337248f0ca7868
BayBayMX <growthgains.100m@gmail.com>
2025-05-04 21:14:56 +0900

    Initial Commit

---
+++ AIMS/AIMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMSApp.swift
+//  AIMS
+//
+//  Created by 尾崎史侑 on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct AIMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
